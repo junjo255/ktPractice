4a92fe8912fc6179e6ad7efa223354213932888a
junjo255 <jun.jo249@gmail.com>
2020-05-05 08:16:15 -0400

    contains duplicates

---
+++ src/containDuplicates/containsDuplicates.java
@@ -0,0 +1,26 @@
+package containDuplicates;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class containsDuplicates {
+    public static void main(String[] args) {
+        int[] list = new int[] {1,2,3,4,5};
+
+        System.out.println(containsDuplicate(list));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> map = new HashSet<>();
+
+        for (int num : nums) {
+            if(map.contains(num)) {
+                return true;
+            }
+            map.add(num);
+        }
+        return false;
+    }
+}
