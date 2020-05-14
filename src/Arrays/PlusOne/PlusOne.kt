f2cfe59d6c91c43f23de4ea325c9ab9f2cfdcf3c
junjo255 <jun.jo249@gmail.com>
2020-05-14 09:26:30 -0400

    plusone

--- src/Arrays/PlusOne/PlusOne.kt
+++ src/Arrays/PlusOne/PlusOne.kt
@@ -1,5 +1,9 @@
 package Arrays.PlusOne
 
+import java.math.BigInteger
+import java.util.stream.Collectors
+import kotlin.streams.toList
+
 /**
  * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
  *
@@ -18,3 +22,36 @@ package Arrays.PlusOne
  *    Output: [4,3,2,2]
  *    Explanation: The array represents the integer 4321.
  */
+
+fun main() {
+//    val nums = intArrayOf(9,8,7,6,5,4,3,2,0)
+    val nums = intArrayOf(7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6)
+    plusOne(nums).forEach{println(it)}
+}
+
+fun plusOne(digits: IntArray): IntArray {
+    val m = MutableList(0){0}
+    val r = digits
+            .reversed()
+            .fold(1){ a, i ->
+                m.add(if (i + a ==10) 0 else i + a)
+                if (i + a == 10) 1 else 0
+            }
+    if (r==1){
+        m.add(1)
+    }
+    return m.reversed().toIntArray()
+}
+
+// Doesn't work on values that are greater than Long
+//fun plusOne(digits: IntArray) : IntArray {
+//    var str = ""
+//    var resStr = ""
+//
+//    for (value in digits) {
+//        str += value.toString()
+//        print(str.chars().)
+//        resStr = (str.toLong() + 1).toString()
+//    }
+//    return resStr.split("").filter{it.isNotEmpty()}.map{it.toInt()}.toList().toIntArray()
+//}
