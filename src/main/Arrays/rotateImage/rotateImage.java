d34aa1dcc0ff099305347241d9398e3b6097f9dc
junjo255 <jun.jo249@gmail.com>
2020-05-21 09:02:12 -0400

    fix

--- src/main/Arrays/rotateImage/rotateImage.java
+++ src/main/Arrays/rotateImage/rotateImage.java
@@ -4,12 +4,6 @@ import static java.util.Arrays.stream;
 
 public class rotateImage {
     public static void main(String[] args) {
-        int[][] matrix = new int[][] {
-                {5, 1, 9, 11},
-                {2, 4, 8, 10},
-                {13, 3, 6, 7},
-                {15,14,12,16}
-        };
         // Expected output
         //            [
         //            [15,13, 2, 5],
@@ -17,16 +11,21 @@ public class rotateImage {
         //            [12, 6, 8, 9],
         //            [16, 7,10,11]
         //            ]
+        int[][] matrix = new int[][] {
+                {5, 1, 9, 11},
+                {2, 4, 8, 10},
+                {13, 3, 6, 7},
+                {15,14,12,16}
+        };
 
         stream(rotate(matrix)).forEach(val -> stream(val).forEach(v -> System.out.println(v)));
-
     }
 
     public static int[][] rotate(int[][] matrix) {
         int length = matrix.length;
 
         for (int col = 0 ; col < length ; col++) {
-            for (int row = 0; row < length ; row++) {
+            for (int row = 0; row < length /2 ; row++) {
                 int temp = matrix[row][col];
                 matrix[row][col] = matrix[length - row - 1][col];
                 matrix[length - row - 1][col] = temp;
